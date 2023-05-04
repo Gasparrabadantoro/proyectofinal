d217f356e784ed34c4c129716aa79d253ef2f017
Gasparrabadantoro <115066281+Gasparrabadantoro@users.noreply.github.com>
2023-05-04 20:32:00 +0200

    6

--- TresEnRaya/jsTresEnRaya.js
+++ TresEnRaya/jsTresEnRaya.js
@@ -1,38 +1,94 @@
 
-let turn =true
-const botonlista=document.querySelectorAll('button')
-const moverArray =new Array(9).fill(null)
+/*let jugador = 'X';
+let tablerillo = ['', '', '', '', '', '', '', '', ''];
+// Funcion para cambiar el colorcete del div si el puntero esta encima 
 
-function ganador(i,j,k){
-    if (moverArray[i]==moverArray[j]&& moverArray[k]==moverArray[j]&& moverArray[i]
-!=null)
-return true
-
-return false
+function encima(id) {
+    // Esto es para modificiar algun elemento (id) del html
+    let elem = document.getElementById(id)
+    elem.style.background = "green";
 }
 
-botonlista.forEach((btn,index)=>{
-    btn.addEventListener('click',event =>{
-        event.target.textContent =turn ? 'X' :'O'
+// Funcion para cambiar el color al salir 
 
-        moverArray[index]= turn
-        if(ganador(0,1,2)||
-        ganador(0,1,2)||
-        ganador(3,4,5)||
-        ganador(6,7,8)||
-        ganador(0,3,6)||
-        ganador(1,4,7)||
-        ganador(2,5,8)||
-        ganador(0,4,8)||
-        ganador(2,4,6))
+function salir(id) {
+    let elem = document.getElementById(id)
+    elem.style.background = "white";
 
-        document.querySelector('#winner')
-        .textContent = `Ganó "${turn?'X':'O'}"`
-        
-        turn =!turn
+}
+
+// colocar marca x u o 
+function colocar(id) {
+    let elem = document.getElementById(id);
+    // estilo de color a lo que tu quieras escribir 
+    elem.innerHTML = "<p style=' color:pink;'><b>" + jugador + "</b></p>";
+    // tablero[id] seria las ids del tablero que he creado 
+    tablero[id] = jugador;
+
+    // aqui verificamos el ganador 
+
+if (winner()) {
+    alert(`El jugador mas loco de la loqueria ha sido ${jugador}.`);
+    return;
+}
+
+jugador = jugador == 'X' ? 'O' : 'X';
+}
+
+function winner() {
+    const combinacionesGanadoras = [['p1', 'p2', 'p3'],
+    ['p4', 'p5', 'p6'],
+    ['p7', 'p8', 'p9'],
+    ['p1', 'p4', 'p7'],
+    ['p2', 'p5', 'p8'],
+    ['p3', 'p6', 'p9'],
+    ['p1', 'p5', 'p9'],
+    ['p3', 'p5', 'p7']
+    ];
+
+    //for para recorrer esas combinaciones 
+
+    for (let i = 0; i < combinacionesGanadoras.length; i++) {
+        const [a, b, c] = combinacionesGanadoras[i];
+
+        if (tablerillo[a] == tablerillo[b] && tablerillo[b] == tablerillo[c] && tablerillo[a]
+            != '') {
+            return tablerillo[a];
+        }
+
+    }
+    return null;
+}
+
+*/
+let turn = true;
+const botonlista = document.querySelectorAll('button');
+const moverArray = new Array(9).fill(null);
+
+function ganador(i, j, k) {
+    if (moverArray[i] == moverArray[j] && moverArray[k] == moverArray[j] && moverArray[i] != null) {
+        return true;
+    }
+    return false;
+}
+
+botonlista.forEach((btn, index) => {
+    btn.addEventListener('click', event => {
+        event.target.textContent = turn ? 'X' : 'O';
+        moverArray[index] = turn ? 'X' : 'O';
+        if (ganador(0, 1, 2) ||
+            ganador(3, 4, 5) ||
+            ganador(6, 7, 8) ||
+            ganador(0, 3, 6) ||
+            ganador(1, 4, 7) ||
+            ganador(2, 5, 8) ||
+            ganador(0, 4, 8) ||
+            ganador(2, 4, 6)) {
+            document.querySelector('#winner').textContent = `Ganó ${turn ? 'X' : 'O'}`;
+        }
+        turn = !turn;
     })
-
-})
+});
 /*const botonPulsado=  (b)=>{
     const boton=b.target ;
  
