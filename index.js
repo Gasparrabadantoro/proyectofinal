dc418ea92a1ec61f27972a674d9fe4a5799319ff
Gaspar Rabadan <gasparrabadan90@gmail.com>
2023-05-25 12:40:15 +0200

    2

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function alerta1 () {
 
+    alert('Nombre: Gaspar Rabadán Toro \n- 1 DAM -\n Web Portafolio, pincha en "Portafolio" para verlo ')
+}
+
+function alerta2 () {
+
     alert('Nombre: Gaspar Rabadán Toro \n- 1 DAM -\n Web Portafolio ')
 }
