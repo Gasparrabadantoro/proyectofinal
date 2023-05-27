7ee5b19608c58498a78b17f0121f3d354a5457c4
Gasparrabadantoro <115066281+Gasparrabadantoro@users.noreply.github.com>
2023-05-27 12:57:15 +0200

    3

---
+++ TresEnRaya/modoOscuro.js
@@ -0,0 +1,26 @@
+
+const toggleButton = document.getElementById('botonOscuro');
+
+
+toggleButton.addEventListener('click', function() {
+    // Verificar si se está en modo día o modo noche
+    if (document.body.classList.contains('dark-mode')) {
+        // Cambiar a modo día
+        document.body.classList.remove('dark-mode');
+        // Guardar preferencia en localStorage
+        localStorage.setItem('modo', 'dia');
+    } else {
+        // Cambiar a modo noche
+        document.body.classList.add('dark-mode');
+        // Guardar preferencia en localStorage
+        localStorage.setItem('modo', 'noche');
+    }
+});
+
+
+window.addEventListener('load', function() {
+    const modoPreferido = localStorage.getItem('modo'); 
+    if (modoPreferido === 'noche') {
+        document.body.classList.add('dark-mode');
+    }
+});
